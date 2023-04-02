ad451a3f1d32b11f53ee15b8092ecba260cc6802
Desmends27 <desmondmends27@gmail.com>
2023-04-02 00:04:00 +0000

    betty error

--- nop.c
+++ nop.c
@@ -5,6 +5,6 @@
  * @line_number: line number
  *
  */
-void nop(__attribute__((unused))stack_t **stack, unsigned int line_number)
+void nop(__attribute__((unused))stack_t **stack,__attribute__((unused)) unsigned int line_number)
 {
 }
